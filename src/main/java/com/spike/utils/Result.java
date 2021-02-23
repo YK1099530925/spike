0c99b5053b70dfa24d1e0760fc6c7274bb1d95d6
YK1099530925 <1099530925@qq.com>
2021-02-23 17:30:19 +0800

    添加返回类型

--- src/main/java/com/spike/utils/Result.java
+++ src/main/java/com/spike/utils/Result.java
@@ -7,4 +7,14 @@ public class Result<T> {
     private int code;
     private String msg;
     private T data;
+
+    public static <T> Result<T> ok(T data){
+        return new Result<>(data);
+    }
+
+    private Result(T data){
+        this.code = 0;
+        this.msg = "success";
+        this.data = data;
+    }
 }
